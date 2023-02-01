20d538f6e0cac4ae83fc8a65ac6f925bbea2c587
GlennWSo <sondellglenn@gmail.com>
2023-02-01 10:50:23 +0100

    added play/pause

--- www/index.js
+++ www/index.js
@@ -73,12 +73,35 @@ const drawCells = () => {
   ctx.stroke();
 }
 
+let animationId = null;
 const renderLoop = () => {
   world.tick();
   drawGrid();
   drawCells();
-  requestAnimationFrame(renderLoop);
+   animationId = requestAnimationFrame(renderLoop);
+}
+const isPaused = () => {
+  return animationId === null;
 }
 
+const playButton = document.getElementById("play-pause");
 
-requestAnimationFrame(renderLoop);
+const play = () => {
+  playButton.textContent = '⏸'
+  renderLoop();
+};
+const pause = () => {
+  playButton.textContent = '▶'
+  cancelAnimationFrame(animationId);
+  animationId = null;
+};
+
+playButton.addEventListener("click", event => {
+  if (isPaused()) {
+    play();
+  } else {
+    pause();
+  }
+});
+
+play();
